0b1e447e51d4f2fbbe4b15006a0e863ecc314e1c
VitorLinas <103148031+VitorLinas@users.noreply.github.com>
2022-07-03 21:51:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -27,8 +27,14 @@ function enviar(){
 
 
 
-
-
+function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.className === "menu") {
+      x.className += " responsive";
+    } else {
+      x.className = "menu";
+    }
+  }
 
 
 
